55ee08121f35c270feee175a1e3bf76267138e9a
samhitha-s-rao <samhitha.srao@primordiusglobal.com>
2026-07-13 15:56:58 +0530

    login api integrate

---
+++ client/src/api/axiosInstance.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+
+const axiosInstance = axios.create({
+    baseURL:"https://localhost:5238/api"
+});
+
+
+axiosInstance.interceptors.request.use(
+(config)=>{
+
+    const token =
+        localStorage.getItem("token") ||
+        sessionStorage.getItem("token");
+
+
+    if(token)
+    {
+        config.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+
+    return config;
+
+});
+
+
+export default axiosInstance;
