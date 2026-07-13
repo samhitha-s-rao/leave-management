a888970d908c93eb28b97ce2f1a9783bf04639d7
Yashaswini-2824 <yashaswini@primordiusglobal.com>
2026-07-13 16:18:26 +0530

    Implement holiday management with API integration and update connection string

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea7e5048e5cb1cb53796788e6c6804098435be6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55ee08121f35c270feee175a1e3bf76267138e9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
