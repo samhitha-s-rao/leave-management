d9de7830128f1587e29c1d14d31c9758f45eb247
samhitha-s-rao <samhitha.srao@primordiusglobal.com>
2026-07-23 15:28:35 +0530

    Simplify Swagger middleware usage

--- server/Program.cs
+++ server/Program.cs
@@ -149,12 +149,8 @@ var app = builder.Build();
 // Middleware
 // --------------------
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowReact");
